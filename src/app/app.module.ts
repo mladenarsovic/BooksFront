60b03db3dfd74cb3cf2ba910f1210e8837b73a58
Mladen <djoml8bre@gmail.com>
2018-02-21 01:17:25 +0100

    Izgenerisane komponente i registrovane, bootstrap downgrade-ovan na beta.2 verziju zbog greske u modulu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BookListComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule
